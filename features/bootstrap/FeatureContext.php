7bcd09411e9e22bef2d0ce3317853c90d6ccdd26
Ricardo Ferreira <ricardo.maurici@hotmail.com>
2015-07-21 18:26:08 -0300

    behat example

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,49 @@
+<?php
+//features/boostrap/FeaturesContext.php
+
+require ("PHPadder.php");
+
+use Behat\Behat\Context\BehatContext, Behat\Behat\Exception\PendingException;
+
+use Behat\Gherkin\Node\PuStringNode, Behat\Gherking\Node\TableNode;
+
+class FeatureContext extends BehatContext
+{
+    private $Adder;
+
+    /**
+    * @Given /^I have the number (\d+) and the number (\d+)$/
+    */
+    public function iHaveTheNumberAndTheNumber($a, $b)
+    {
+        $this->Adder = new PHPadder($a, $b);
+    }
+
+    /**
+     * @When /^I add them together$/
+     */
+    public function iAddThemTogether()
+    {
+        $this->Adder->add();
+    }
+
+    /**
+     * @Given /^I have a third number of (\d+)$/
+     */
+    public function iHaveAThirdNumberOf($c)
+    {
+        $this->Adder->setThird($c);
+    }
+
+    /**
+     * @Then /^I should get (\d+)$/
+     */
+    public function iShouldGet($sum)
+    {
+        //echo $this->Adder->nome;
+        if ($this->Adder->getSum() != $sum) {
+            throw new Exception("Valor da soma atual: " . $this->Adder->getSum());
+        }
+        $this->Adder->display();
+    }
+}
